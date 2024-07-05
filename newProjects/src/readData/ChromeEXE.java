839c224e5f22f7ac8fb13a585070b8e09c58e946
SurajTesters <deepakkumar7714@outlook.com>
2024-07-05 07:50:32 -0700

    FlipKart URL added.

--- newProjects/src/readData/ChromeEXE.java
+++ newProjects/src/readData/ChromeEXE.java
@@ -8,5 +8,6 @@ public class ChromeEXE
 	public static void main(String[] args) {
 		ChromeEXE ob = new ChromeEXE();
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
 	}
 }
